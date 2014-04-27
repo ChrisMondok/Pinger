8d2d762d06b080867e50e92f086d7ed3a2405114
Chris Mondok <chris.mondok@gmail.com>
2014-04-27 14:46:07 -0400

    Made water blocks transparent.
    This improves lose animation

--- Water.js
+++ Water.js
@@ -3,6 +3,6 @@ var Water = extend(Pawn);
 Water.prototype.draw = function(ctx) {
 	if(this.gameBoard.dirt[this.x][this.y] !== false)
 		return;
-	ctx.fillStyle = "#0069d5";
+	ctx.fillStyle = "rgba(0, 105, 213, 0.5)";
 	ctx.fillRect(this.x * GRIDSIZE, this.y * GRIDSIZE, GRIDSIZE, GRIDSIZE);
 };
